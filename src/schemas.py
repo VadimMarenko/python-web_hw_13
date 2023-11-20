25376ad39318e44d62125be104ab1eec6a72e568
VadimMarenko <130446385+VadimMarenko@users.noreply.github.com>
2023-11-20 15:08:29 +0200

    Update schemas.py
    add RequestEmail

--- src/schemas.py
+++ src/schemas.py
@@ -37,18 +37,11 @@ class UserEmailModel(BaseModel):
     email: EmailStr
 
 
-# class UserResponse(BaseModel):
-#     id: int
-#     username: str
-#     email: str
-#     avatar: str
-#     role: Role
-
-#     class Config:
-#         from_attributes = True
-
-
 class TokenModel(BaseModel):
     access_token: str
     refresh_token: str
     token_type: str = "bearer"
+
+
+class RequestEmail(BaseModel):
+    email: EmailStr
