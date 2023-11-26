da90c05e3201c97f0023491ee848c9e7a50ab9f5
VadimMarenko <130446385+VadimMarenko@users.noreply.github.com>
2023-11-26 11:31:10 +0200

    Update register.js

--- static/javascript/register.js
+++ static/javascript/register.js
@@ -1,23 +1,34 @@
-const form = document.forms[0]
+async function submitForm() {
+    const form = document.forms[0];
 
-form.addEventListener('submit', async (e) => {
-  e.preventDefault()
-  const response = await fetch('http://localhost:8000/api/auth/signup', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams({
-        username: form.username.value,
-        email: form.email.value,
-        password: form.password.value,
-    }),
-  })
-  console.log(response.status, response.statusText)
-  if (response.status === 200) {
-    result = await response.json()
-    localStorage.setItem('accessToken', result.access_token)
-    localStorage.setItem('refreshToken', result.refresh_token)
-    window.location = '/index.html'
-  }
-})
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        
+        const formData = {
+            first_name: form.first_name.value,
+            last_name: form.last_name.value,
+            username: form.username.value,
+            email: form.email.value,
+            password: form.password.value,
+            born_date: form.born_date.value,
+            description: form.description.value,
+        };
+
+        const response = await fetch('http://localhost:8000/api/auth/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        });
+
+        console.log(response.status, response.statusText);
+
+        if (response.ok) {
+            const result = await response.json();
+            alert(`Congratulations, ${result.user.username}! Your account is created.`);
+            // Redirect to "/"
+            window.location = '/';
+        }
+    });
+}
